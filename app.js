443f546b4c3162191f098fa1eebe550eda4ed4fb
Rao-Lohitashva <95473887+Rao-Lohitashva@users.noreply.github.com>
2022-03-30 00:56:02 +0530

    this is mine

--- app.js
+++ app.js
@@ -2,20 +2,14 @@ const express=require('express');
 const bodyparser=require('body-parser');
 const mongoose=require('mongoose');
 const userRoute=require('./routes/user.route')
-const cors=require('cors')
 const port=process.env.PORT ||3000;
 const app=express();
 
-
-
-
-
-app.use(cors);
 mongoose.connect('mongodb+srv://lucky:y5QvxONsaSK9OD01@mycluster.rhrpn.mongodb.net/users?retryWrites=true&w=majority',()=>{
     console.log("connected")
 })
 
-app.use(cors);
+
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(bodyparser.json());
 
