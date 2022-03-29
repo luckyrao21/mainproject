be19ca1aa5528fa658ecbbbc93a4bb6864073c0d
Rao-Lohitashva <95473887+Rao-Lohitashva@users.noreply.github.com>
2022-03-29 18:38:59 +0530

    this is my

--- app.js
+++ app.js
@@ -10,12 +10,13 @@ const port=process.env.PORT||3000;
 
 mongoose.connect('mongodb+srv://lucky:y5QvxONsaSK9OD01@mycluster.rhrpn.mongodb.net/users?retryWrites=true&w=majority',()=>{
     console.log("connected")
+})
 
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(bodyparser.json());
 
 app.use('/',userRoute);
-})
+
 app.listen(port,()=>{
     console.log('server is running')
 })
