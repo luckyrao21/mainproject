3f9bf08424759788116ced5db6a1e89b57436abe
Rao-Lohitashva <95473887+Rao-Lohitashva@users.noreply.github.com>
2022-03-30 12:08:47 +0530

    this is mine

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express=require('express');
 const bodyparser=require('body-parser');
 const mongoose=require('mongoose');
 const userRoute=require('./routes/user.route')
-const cors=require('cors')
 const port=process.env.PORT ||3000;
 const app=express();
+const cors=require('cors')
 
 mongoose.connect('mongodb+srv://luckyrao:1234@cluster1.bvxkm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(()=>{
     console.log("success")
